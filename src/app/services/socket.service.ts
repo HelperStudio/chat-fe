ee7fa8f91157d939e1ff1fb87e6fb70ebf4138bc
hspoluektova <poluektova@helper-studio.com>
2018-06-13 22:49:19 +0700

    socket events for user list

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -1,38 +1,47 @@
-import { Injectable } from '@angular/core';
-import { Message } from '../models/message';
+import { Injectable } from "@angular/core";
+import { Message } from "../models/message";
+import { User } from "../models/user";
 
-import { IdentityService } from './identity.service';
+import { IdentityService } from "./identity.service";
 
-import { AppConfig } from '../app.config';
+import { AppConfig } from "../app.config";
 
-import * as io from 'socket.io-client';
+import * as io from "socket.io-client";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root"
 })
-
-
 export class SocketService {
+  socket: any;
+  messages: Array<Message>;
+  users: Array<User>;
 
-    socket: any;
-    messages: Array<Message>;
-    protected apiServer = AppConfig.settings.apiServer;
+  protected apiServer = AppConfig.settings.apiServer;
 
-    constructor(private identityService: IdentityService) {
-        this.messages = [];
-    }
+  constructor(private identityService: IdentityService) {
+    this.messages = [];
+    this.users = [];
+  }
 
-    initialize() {
-    
-        this.socket = io.connect(this.apiServer.url + "?id=" + this.identityService.user);
+  initialize() {
+    this.socket = io.connect(
+      this.apiServer.url + "?id=" + this.identityService.user
+    );
 
-        this.socket.on('message', (msg: Message) => {
-            this.messages.push(msg);
-        });
-    }
+    this.socket.on("message", (msg: Message) => {
+      this.messages.push(msg);
+    });
 
-    sendMessage(msg:Message){
-        this.socket.emit("message", msg);
-    }
+    this.socket.on("online", (user: User) => {
+      this.users.push(user);
+    });
 
+    this.socket.on("offline", (user: User) => {
+      this.users = this.users.filter(x => x.socketId != user.socketId);
+    });
+  }
+
+  sendMessage(msg: Message) {
+    this.socket.emit("message", msg);
+  }
 }
--- src/app/userlist/userlist.component.ts
+++ src/app/userlist/userlist.component.ts
@@ -1,35 +1,36 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit } from "@angular/core";
 
-import { UserService } from '../services/user.service';
+import { UserService } from "../services/user.service";
+import { SocketService } from "../services/socket.service";
 
-import { User } from '../models/user'
-
+import { User } from "../models/user";
 
 @Component({
-  selector: 'userlist',
-  templateUrl: './userlist.component.html',
-  styleUrls: ['./userlist.component.css']
+  selector: "userlist",
+  templateUrl: "./userlist.component.html",
+  styleUrls: ["./userlist.component.css"]
 })
 export class UserListComponent {
- 
-  users: Array<User>;
-
-  constructor(private userService: UserService){
+  constructor(
+    private userService: UserService,
+    private socketService: SocketService
+  ) {
     this.getUsers();
   }
 
-  getUsers(){
-    this.userService.getConfigResponse()
-    // resp is of type `HttpResponse<Config>`
-    .subscribe(resp => {
-        this.users = resp.body;
-      // display its headers
-      //const keys = resp.headers.keys();
-      //this.headers = keys.map(key =>
+  getUsers() {
+    this.userService
+      .getConfigResponse()
+      // resp is of type `HttpResponse<Config>`
+      .subscribe(resp => {
+        this.socketService.users = resp.body;
+        // display its headers
+        //const keys = resp.headers.keys();
+        //this.headers = keys.map(key =>
         //`${key}: ${resp.headers.get(key)}`);
 
-      // access the body directly, which is typed as `Config`.
-      //this.config = { ... resp.body };
-    });
+        // access the body directly, which is typed as `Config`.
+        //this.config = { ... resp.body };
+      });
   }
 }
